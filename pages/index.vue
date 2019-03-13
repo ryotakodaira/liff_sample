11bade93ca912c9670cd82da6eeb2b260e883a75
RyotaKodaira <l98603345@gmail.com>
2019-03-13 15:48:00 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="container">
+    <p class="line-id">LINE ID：{{ lineId }}</p>
+    <div class="form">
+      <div class="control">
+        <input class="input" type="text" placeholder="お名前" v-model="formData.name">
+      </div>
+      <button class="button is-info is-fullwidth" @click="onSubmit()">送信する</button>
+      <button class="button is-light is-fullwidth" @click="handleCancel()">キャンセル</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: ''
+      },
+      lineId: null
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (!this.canUseLIFF()) {
+        return
+      }
+
+      window.liff
+        .sendMessages([
+          {
+            type: 'text',
+            text: `お名前：\n${this.formData.name}`
+          },
+          {
+            type: 'text',
+            text: '送信が完了しました'
+          }
+        ])
+        .then(() => {
+          window.liff.closeWindow()
+        })
+        .catch(e => {
+          window.alert('Error sending message: ' + e)
+        })
+    },
+    handleCancel() {
+      if (!this.canUseLIFF()) {
+        return
+      }
+      window.liff.closeWindow()
+    },
+    canUseLIFF() {
+      return navigator.userAgent.indexOf('Line') !== -1 && window.liff
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  padding: 20px;
+  min-height: 100vh;
+}
+
+.line-id {
+  margin-bottom: 30px;
+}
+
+.form > * {
+  margin-bottom: 10px;
+}
+</style>
